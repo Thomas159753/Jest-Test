fa9ae2f38c01b538ade85fbe25b8d508466a7e5f
Thomas159753 <99398525+Thomas159753@users.noreply.github.com>
2024-04-16 08:31:07 +1000

    more test added
    capitalize,
    reverse string,
    calculator

--- Testing-Practice.js
+++ Testing-Practice.js
@@ -1,13 +1,33 @@
-function sum(a, b) {
-    return a + b;
-  }
-
 function capitalize (string){
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+function reverseString (string){
+  return string.split('').reduce((acc, char) => char + acc, "")
+}
+
+const calculator = {
+  add(a,b){
+    const result = a + b;
+    return Math.ceil(result * 100) / 100;
+  },
+
+  subtract(a,b){
+    return a - (b);
+    // return Math.ceil(result * 100) / 100;
+  }
+
+  // divide(){
+
+  // }
+
+  // multiply(){
+
+  // }
+}
 
 module.exports = {
-  sum: sum,
-  capitalize: capitalize
+  capitalize: capitalize,
+  reverseString: reverseString,
+  calculator: calculator
 };
--- Testing-Practice.test.js
+++ Testing-Practice.test.js
@@ -1,26 +1,48 @@
 const {
-  sum,
-  capitalize
+  capitalize,
+  reverseString,
+  calculator
 } = require('./Testing-Practice');
 
-//Sum tests
+////Calculator tests
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
-test('adds -1 + 2 to equal 1', () => {
-  expect(sum(-1, 2)).toBe(1);
+  expect(calculator.add(1, 2)).toBe(3);
 });
 test('adds 0 + 2 to equal 2', () => {
-  expect(sum(0, 2)).toBe(2);
+  expect(calculator.add(0, 2)).toBe(2);
 });
 test('adds 0 + -2 to equal -2', () => {
-  expect(sum(0, -2)).toBe(-2);
+  expect(calculator.add(0, -2)).toBe(-2);
+});
+test('adds 0.0001 + 1 to equal 1.01', () => {
+  expect(calculator.add(0.001, 1)).toBe(1.01);
+});
+
+test('subtract 2 - 1 to equal 1', () => {
+  expect(calculator.subtract(2, 1)).toBe(1);
+});
+test('subtract 1 - 2 to equal -1', () => {
+  expect(calculator.subtract(1, 2)).toBe(-1);
+});
+test('subtract -2.0001 - 1 to equal -', () => {
+  expect(calculator.subtract(-2, -1)).toBe(-3);
 });
 
 //Capitalize First Character
 
 test('Capitalize: Normal string', () => {
-  expect(capitalize('hello world')).toBe('Hello world')
+  expect(capitalize('hello world')).toBe('Hello world');
 });
-test('Capitalize: Already Capitalize')
+test('Capitalize: Already Capitalize', () => {
+  expect(capitalize('Hello world')).toBe('Hello world');
+});
+
+//Reverse String Test
+
+test('ReverseString single word: Hello to be olleH', () =>{
+  expect(reverseString('Hello')).toBe('olleH');
+});
+test('ReverseString multiple words: Hello world to be dlrow olleH', () =>{
+  expect(reverseString('Hello world')).toBe('dlrow olleH');
+});
 
